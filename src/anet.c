37d377b287b9d46be75d3ebd2ebc8c06baa11a68
Hao Liu <liuhaobupt@gmail.com>
2012-02-26 16:42:01 +0800

    add anetListen fucntion

--- src/anet.c
+++ src/anet.c
@@ -35,3 +35,19 @@ static int anetCreateSocket(char *err, int domain)
     return s;
 }
 
+static int anetListen(char *err, int s, struct sockaddr *sa, socklen_t len)
+{
+    if(bind(s,sa,len) == -1)
+    {
+        anetSetError(err, "bind: %s", strerror(errno));
+        close(s);
+        return ANET_ERR;
+    }
+    if (listen(s, 511) == -1)
+    {
+        anetSetError(err, "listen: %s", strerror(errno));
+        close(s);
+        return ANET_ERR;
+    }
+    return ANET_OK;
+}

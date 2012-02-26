a54d1e671ff5c8ccced79715d4d37d6c48cb2a72
Hao Liu <liuhaobupt@gmail.com>
2012-02-26 16:52:46 +0800

    add anetTcpServer fucntion in anet.c

--- src/anet.c
+++ src/anet.c
@@ -51,3 +51,26 @@ static int anetListen(char *err, int s, struct sockaddr *sa, socklen_t len)
     }
     return ANET_OK;
 }
+
+int anetTcpServer(char *err, int port, char *bindaddr)
+{
+    int s;
+    struct sockaddr_in sa;
+
+    if ((s = anetCreateSocket(err, AF_INET)) == ANET_ERR)
+        return ANET_ERR;
+    
+    memset(&sa, 0, sizeof(sa));
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(port);
+    sa.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bindaddr && inet_aton(bindaddr, &sa.sin_addr) == 0)
+    {
+        anetSetError(err, "invalid bind address");
+        close(s);
+        return ANET_ERR;
+    }
+    if (anetListen(err,s,(struct sockaddr *)&sa,sizeof(sa)) == ANET_ERR )
+        return ANET_ERR;
+    return s;
+}
